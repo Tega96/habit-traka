052e27e89299539995223056ed3040a3ab3f10da
Otega Otite <otiteotega@gmail.com>
2026-04-28 19:19:55 +0100

    Protect dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,37 @@
+"use client"
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { isAuthenticated, logoutUser } from '@/lib/auth';
 import { memo } from 'react';
 
+
 const Dashboard = () => {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
-  );
+
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!isAuthenticated()) {
+            router.replace('/login');
+        }
+    }, [router]);
+
+    const handleLogout = () => {
+        logoutUser();
+        router.push('/login')
+    }
+
+    return (
+        <div className="p-8">
+            <h1 className="text-2xl font-bold">Dashboard</h1>
+            <button
+                onClick={handleLogout}
+                className="mt-4 bg-red-500 text-white p-2 rounded"
+            >
+                Logout
+            </button>
+            <p className="mt-4">Habit tracking ui</p>
+        </div>
+    );
 };
 
 export default memo(Dashboard);
