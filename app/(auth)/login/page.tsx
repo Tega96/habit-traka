bac2b6a5eb96fd87d22fbc80eebf5440b9fe13c4
Otega Otite <otiteotega@gmail.com>
2026-04-28 18:59:26 +0100

    Build the sign-in and sign-up ui

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,11 +1,66 @@
+"use client"
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import { loginUser } from '@/lib/auth';
 import { memo } from 'react';
 
+
 const Login = () => {
-  return (
-    <div>
-      <h2>Login</h2>
-    </div>
-  );
+    const router = useRouter();
+    
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState('')
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        setError("");
+
+        const loggedIn = loginUser(email, password);
+        if (loggedIn) {
+            router.push("/dashboard")
+        } else {
+            setError("Invalid email or password")
+        }
+    };
+
+        
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="bg-white p-8 rounded shadow-md w-96">
+                <h1 className="text-2xl font-bold mb-6">Login</h1>
+                {error && <p className="text-red-500 mb-4">{error}</p>}
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        type="email"
+                        placeholder='Email'
+                        className='w-full p-2 border rounded mb-4'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)} 
+                        required
+                    />
+                    <input 
+                        type="password"
+                        placeholder='Password'
+                        className='w-full p-2 border rounded mb-4'
+                        value={password}
+                        onChange={(e) => setEmail(e.target.value)} 
+                        required
+                    />
+                    <button type="submit" 
+                        className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600">
+                            Login
+                        </button>
+                </form>
+                <p className="mt-4 text-center text-sm">
+                    No account? {" "}
+                    <Link href={"/signup"} className="text-blue-500">Sign up</Link>
+                </p>
+            </div>
+        
+        </div>
+    );
 };
 
 export default memo(Login);
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,11 +1,76 @@
+"use client"
+
 import { memo } from 'react';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import { createUser, loginUser } from '@/lib/auth';
+
+
 
 const SignUp = () => {
-  return (
-    <div>
-      <h2>SignUp</h2>
-    </div>
-  );
+
+    const router = useRouter();
+    const [email, setEmail] = useState("");
+    const [password, setPassoword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        setError("")
+
+        const created = createUser(email, password);
+        if (!created) {
+            setError("User already exists");
+            return;
+        }
+
+        const loggedIn = loginUser(email, password);
+        if (loggedIn) {
+            router.push('/dashboard')
+        } else {
+            setError("Login failed after signup")
+        };
+    };
+
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="bg-white p-8 rounded shadow-md w-96">
+                <h1 className="text-2xl font-bold mb-6">Sign up </h1>
+                {error && <p className="text-red-500 mb-4">{error}</p>}
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        type="email"
+                        placeholder="Email"
+                        className="w-full p-2 border rounded mb-4"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required 
+                    />
+                    <input 
+                        type="password"
+                        placeholder="Password"
+                        className="w-full p-2 border rounded mb-4"
+                        value={password}
+                        onChange={(e) => setPassoword(e.target.value)}
+                        required 
+                    />
+                    <button
+                        type="submit"
+                        className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+                    >
+                        Sign Up
+                    </button>
+                </form>
+                <p className="mt-4 text-center text-sm">
+                    Already have an account?{" "}
+                    <Link href="login" className="text-blue-500">
+                        Login
+                    </Link>
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default memo(SignUp);
